0f7791a45c4512fe6e096330c078f857c01544e8
yusancky <yusancky@hotmail.com>
2025-09-14 18:55:24 +0800

    Apply suggestions from code review

--- scripts/get-files-to-test.py
+++ scripts/get-files-to-test.py
@@ -35,13 +35,10 @@ if __name__ == "__main__":
             changed_extnamed_codes = " ".join(
                 filter(lambda x: x.endswith(extname), changed_codes)
             )
-            print("changed_extnamed_codes" + changed_extnamed_codes)
             if changed_extnamed_codes == "":
-                changed_extnamed_codes = "False"
-                print("changed_extnamed_codes is now False!")
+                changed_extnamed_codes = "None"
             with open(os.environ.get("GITHUB_OUTPUT"), "w") as f:
-                print(f"TEST_{extname.upper()}_FILES={changed_extnamed_codes}")
-                f.write(f"TEST_{extname.upper()}_FILES={changed_extnamed_codes}")
+                f.write(f"TEST_{extname[1:].upper()}_FILES={changed_extnamed_codes}")
     else:
         for extname in extnames:
             all_extnamed_codes = []
