fdf4c3e3c248bb0580d434fda98247bbd5673bb4
yusancky <yusancky@hotmail.com>
2025-09-14 18:31:14 +0800

    Update scripts/get-files-to-test.py

--- scripts/get-files-to-test.py
+++ scripts/get-files-to-test.py
@@ -48,6 +48,8 @@ if __name__ == "__main__":
                         all_extnamed_codes.append(os.path.join(root, file))
             all_extnamed_codes = " ".join(all_extnamed_codes)
             if all_extnamed_codes == "":
+                print("ccc")
                 all_extnamed_codes = "None"
             with open(os.environ.get("GITHUB_OUTPUT"), "w") as f:
+                print(f"TEST_{extname.upper()}_FILES={all_extnamed_codes}")
                 f.write(f"TEST_{extname.upper()}_FILES={all_extnamed_codes}")
