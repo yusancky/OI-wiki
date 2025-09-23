268185bccbf2e8a6403c42c1aaee96f148f81e06
yusancky <yusancky@hotmail.com>
2025-09-24 00:21:07 +0800

    更新 correctness.py

--- scripts/check-example-codes/cpp/correctness.py
+++ scripts/check-example-codes/cpp/correctness.py
@@ -18,7 +18,7 @@ def check_correctness(test_file):
 
     if not os.path.exists(test_file):
         print(f"File {test_file} does not exist\n::endgroup::")
-        return 2, f"❌ 文件 {test_file} 不存在"
+        return 1, f"❌ 文件 {test_file} 不存在"
 
     auxfiles = " ".join(get_auxfiles(test_file))
     executable = test_file.split(".")[0]
@@ -29,7 +29,7 @@ def check_correctness(test_file):
         print(
             f"CE\n::endgroup::\n::error file={test_file},title=Compile Error::Compile Error with error code {result.returncode}"
         )
-        return 2, f"❌ 文件 {test_file} 编译错误"
+        return 1, f"❌ 文件 {test_file} 编译错误"
     print("OK")
 
     in_file, out_file, ans_file = code2examples(test_file)
@@ -39,7 +39,7 @@ def check_correctness(test_file):
         )
         return (
             1,
-            f"⚠️ 文件 {test_file} 的样例输入 {in_file} 或样例输出 {ans_file} 不存在，不校验输出结果",
+            f"⚠️ 文件 {test_file} 的样例输入 {in_file} 或样例输出 {ans_file} 不存在，无法校验输出结果。请上传对应样例，或将没有样例的代码直接嵌入 Markdown 文件的代码块中。",
         )
 
     print(f"Runing {executable} with input {in_file}", end=" ")
@@ -50,7 +50,7 @@ def check_correctness(test_file):
         print(
             f"::error file={test_file},title=Runtime Error::Runtime Error with error code: {result.returncode}\n::endgroup::"
         )
-        return 2, f"❌ 文件 {test_file} 运行时错误"
+        return 1, f"❌ 文件 {test_file} 运行时错误"
     print("OK")
 
     check_command = f"diff -b -B {out_file} {ans_file}"
@@ -61,7 +61,7 @@ def check_correctness(test_file):
             f"::error file={test_file},title=Wrong Answer::The output is different to the answer {ans_file}"
         )
         return (
-            2,
+            1,
             f"❌ 文件 {test_file} 输出与答案不同\n    答案：\n    ```\n    {open(ans_file).read().replace(os.linesep, f'{os.linesep}    ')}\n    ```\n    输出：\n    ```\n    {open(ans_file).read().replace(os.linesep, f'{os.linesep}    ')}\n    ```",
         )
     print("Accepted!\n::endgroup::")
@@ -70,13 +70,13 @@ def check_correctness(test_file):
 
 if __name__ == "__main__":
     test_files = os.environ.get("TEST_CPP_FILES").split(" ")
-    cnts = [0, 0, 0]
+    cnts = [0, 0]
     summary = ""
     for test_file in test_files:
         correctness, test_summary = check_correctness(test_file)
         cnts[correctness] += 1
         summary += "- " + test_summary
-    general_summary = f"TOTAL {len(test_files)} TESTS, {cnts[0]} ACCEPTED, {cnts[1]} SKIPPED, {cnts[2]} ERROR"
+    general_summary = f"TOTAL {len(test_files)} TESTS, {cnts[0]} ACCEPTED, {cnts[1]} ERROR/WARNING"
     print(general_summary)
     open(os.environ.get("GITHUB_STEP_SUMMARY"), "w").write(
         f"**{general_summary}**\n\n{summary}"
