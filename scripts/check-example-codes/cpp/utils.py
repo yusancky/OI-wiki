a6c078a6d714e31ad0e0146f9185864d1b2125e8
yusancky <yusancky@hotmail.com>
2025-09-24 09:23:44 +0800

    更新 utils.py

--- scripts/check-example-codes/cpp/utils.py
+++ scripts/check-example-codes/cpp/utils.py
@@ -1,16 +1,6 @@
 import os
 
 
-def code2examples(code_filename):
-    dirname = os.path.dirname(code_filename)
-    basename = os.path.splitext(os.path.basename(code_filename))[0]
-    examples_dir = dirname.replace("code", "examples")
-    in_file = os.path.normpath(os.path.join(examples_dir, basename + ".in"))
-    out_file = os.path.normpath(os.path.join(examples_dir, basename + ".out"))
-    ans_file = os.path.normpath(os.path.join(examples_dir, basename + ".ans"))
-    return in_file, out_file, ans_file
-
-
 def get_auxfiles(cpp_filename):
     dirname = os.path.dirname(cpp_filename)
     basename = os.path.splitext(os.path.basename(cpp_filename))[0]
@@ -20,3 +10,12 @@ def get_auxfiles(cpp_filename):
             if file.split(".")[0] == basename.split(".")[0] and file.endswith(".cpp"):
                 auxfiles.append(os.path.normpath(os.path.join(root, file)))
     return auxfiles
+
+def get_examples(code_filename):
+    dirname = os.path.dirname(code_filename)
+    basename = os.path.splitext(os.path.basename(code_filename))[0]
+    examples_dir = dirname.replace("code", "examples")
+    in_file = os.path.normpath(os.path.join(examples_dir, basename + ".in"))
+    out_file = os.path.normpath(os.path.join(examples_dir, basename + ".out"))
+    ans_file = os.path.normpath(os.path.join(examples_dir, basename + ".ans"))
+    return in_file, out_file, ans_file
