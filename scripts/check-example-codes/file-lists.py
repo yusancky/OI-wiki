4d5ce0b2886be0dc2f0aac48972d47d91a30a4f5
yusancky <yusancky@hotmail.com>
2025-09-24 15:18:42 +0800

    更新 file-lists.py

--- scripts/check-example-codes/file-lists.py
+++ scripts/check-example-codes/file-lists.py
@@ -4,12 +4,18 @@ extnames = [".cpp", ".py"]
 
 
 def check_availability(file):
-    if ".aux" in file:
+    dirname = os.path.dirname(file)
+    basename = os.path.splitext(os.path.basename(file))[0]
+    extname = "." + os.path.splitext(os.path.basename(file))[1]
+    if "." in basename:
+        basename = basename.split(".")[0]
+        if os.path.exists(os.path.join(dirname, basename + extname)):
+            return os.path.normpath(os.path.join(dirname, basename + extname))
+    skip_file = os.path.join(dirname, basename + ".skip_test")
+    if os.path.exists(skip_file):
         return False
-    dirname = os.path.dirname(filename)
-    basename = os.path.splitext(os.path.basename(code_filename))[0]
-    skip_file = os.path.normpath(os.path.join(dirname, basename + ".skip_test"))
-    return not os.path.exists(skip_file)
+    else:
+        return os.path.normpath(os.path.join(dirname, basename + extname)
 
 
 def examples2code(example_file):
@@ -20,7 +26,7 @@ def examples2code(example_file):
     for extname in extnames:
         code_file = os.path.normpath(os.path.join(code_dir, basename + extname))
         if os.path.exists(code_file) and check_availability(code_file):
-            code_files.append(code_file)
+            code_files.append(check_availability(code_file))
     return code_files
 
 
@@ -49,6 +55,6 @@ if __name__ == "__main__":
             all_extnamed_codes = []
             for root, _, files in os.walk("docs"):
                 for file in files:
-                    if file.endswith(extname) and check_availability(file):
-                        all_extnamed_codes.append(os.path.join(root, file))
+                    if file.endswith(extname) and check_availability(os.path.join(root, file)):
+                        all_extnamed_codes.append(check_availability(os.path.join(root, file)))
             output(f"TEST_{extname[1:].upper()}_FILES", " ".join(all_extnamed_codes))
