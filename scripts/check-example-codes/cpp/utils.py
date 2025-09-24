359399b60af8c8fa6ce68e03dc5471a2c4fe02d2
yusancky <yusancky@hotmail.com>
2025-09-24 09:20:25 +0800

    更新 utils.py

--- scripts/check-example-codes/cpp/utils.py
+++ scripts/check-example-codes/cpp/utils.py
@@ -1,6 +1,16 @@
 import os
 
 
+def code2examples(code_filename):
+    dirname = os.path.dirname(code_filename)
+    basename = os.path.splitext(os.path.basename(code_filename))[0]
+    examples_dir = dirname.replace("code", "examples")
+    in_file = os.path.normpath(os.path.join(examples_dir, basename + ".in"))
+    out_file = os.path.normpath(os.path.join(examples_dir, basename + ".out"))
+    ans_file = os.path.normpath(os.path.join(examples_dir, basename + ".ans"))
+    return in_file, out_file, ans_file
+
+
 def get_auxfiles(cpp_filename):
     dirname = os.path.dirname(cpp_filename)
     basename = os.path.splitext(os.path.basename(cpp_filename))[0]
