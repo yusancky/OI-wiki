88a33bd4474aecf2d3efc0ce9c9c91a98078b8da
yusancky <yusancky@hotmail.com>
2025-09-24 09:20:12 +0800

    更新 correctness.py

--- scripts/check-example-codes/cpp/correctness.py
+++ scripts/check-example-codes/cpp/correctness.py
@@ -3,16 +3,6 @@ import subprocess
 from utils import *
 
 
-def code2examples(code_filename):
-    dirname = os.path.dirname(code_filename)
-    basename = os.path.splitext(os.path.basename(code_filename))[0]
-    examples_dir = dirname.replace("code", "examples")
-    in_file = os.path.normpath(os.path.join(examples_dir, basename + ".in"))
-    out_file = os.path.normpath(os.path.join(examples_dir, basename + ".out"))
-    ans_file = os.path.normpath(os.path.join(examples_dir, basename + ".ans"))
-    return in_file, out_file, ans_file
-
-
 def check_correctness(test_file):
     print(f"::group::Test for {test_file}...")
 
