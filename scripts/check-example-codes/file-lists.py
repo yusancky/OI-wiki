7eb6ce67b3d768212ef1e3268cf5618759ea3d18
yusancky <yusancky@hotmail.com>
2025-10-05 14:21:01 +0800

    fix(file-lists.py): white list

--- scripts/check-example-codes/file-lists.py
+++ scripts/check-example-codes/file-lists.py
@@ -5,6 +5,8 @@ extnames = [".cpp", ".py"]
 
 def check_availability(file):
     dirname = os.path.dirname(file)
+    if dirname.split('/')[-1] in ["images"]:
+        return false
     basename = os.path.splitext(os.path.basename(file))[0]
     extname = os.path.splitext(os.path.basename(file))[1]
     if "." in basename:
