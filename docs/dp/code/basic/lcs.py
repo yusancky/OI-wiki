e54c5c4cbe13cc627a4630014481afdb8d3beba9
yusancky <yusancky@hotmail.com>
2026-01-02 01:04:56 +0800

    更新 lcs.py

--- docs/dp/code/basic/lcs.py
+++ docs/dp/code/basic/lcs.py
@@ -7,7 +7,7 @@ def dp(n, m, a, b):
                 f[i][j] = f[i - 1][j - 1] + 1
             else:
                 f[i][j] = max(f[i - 1][j], f[i][j - 1])
-    return f[n][m]
+    return f[n][m + 0]
 
 
 # --8<-- [end:core]
